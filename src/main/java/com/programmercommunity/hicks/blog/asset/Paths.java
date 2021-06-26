57d8e986a92e8792a879b1684aff9c84b6f77d11
programmercommunity-manimaran <83858656+programmercommunity-manimaran@users.noreply.github.com>
2021-06-26 14:29:35 +0530

    Update Paths.java

--- src/main/java/com/programmercommunity/hicks/blog/asset/Paths.java
+++ src/main/java/com/programmercommunity/hicks/blog/asset/Paths.java
@@ -3,9 +3,9 @@ package com.programmercommunity.hicks.blog.asset;
 
 public class Paths {
 	public static class BlogControllerPaths {
-		public static final String SORT_BY_DATE = "/blogs/ "; // sort by date
+		public static final String SORT_BY_DATE = "/blogs/"; // sort by date
 		public static final String SORT_BY_LIKES = "/blogs/toprated/";
-		public static final String SEARCH= "/blogs"; //search
+		public static final String SEARCH= "/blogs/"; //search
 		public static final String FEED= "/blogs/feeds/"; //from followed peoples
 	}
 }
